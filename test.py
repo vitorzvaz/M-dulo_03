faf9dda71dd933a556766628f9712d9a9cbdcd1a
vitorzvaz <vitor_zvs@yahoo.com.br>
2024-06-19 16:32:03 -0300

    Updating to access archives in home

---
+++ test.py
@@ -0,0 +1,19 @@
+import pytest
+import requests
+
+# CRUD test
+BASE_URL = 'http://127.0.0.1:5000'
+tasks = []
+
+# Create test
+def test_create_task():
+    new_task_data = {
+        "title": "Nova tarefa",
+        "description": "Descrição da nova tarefa"
+    }
+    response = requests.post(f"{BASE_URL}/tasks", json = new_task_data)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+    assert "id" in response_json
+    tasks.append(response_json['id'])
