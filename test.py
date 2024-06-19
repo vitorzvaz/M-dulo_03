1655c67932a7a6558f10d8fbfad0c587d5a6d056
vitorzvaz <vitor_zvs@yahoo.com.br>
2024-06-19 19:28:19 -0300

    Finished test.py

--- test.py
+++ test.py
@@ -17,3 +17,56 @@ def test_create_task():
     assert "message" in response_json
     assert "id" in response_json
     tasks.append(response_json['id'])
+
+# Read all test
+def test_get_tasks():
+    response = requests.get(f"{BASE_URL}/tasks")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "tasks" in response_json
+    assert "total_tasks" in response_json
+
+# Read specific test
+
+def test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert task_id == response_json['id']
+
+# Update test
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        payload = {
+            "completed": True,
+            "description": "Descrição atualizada",
+            "title": "Tarefa atualizada"
+        }
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+        assert response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+
+        # Nova requisição da tarefa para checar se foi atualizada
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json['title'] == payload['title']
+        assert response_json['description'] == payload['description']
+        assert response_json['completed'] == payload['completed']
+
+# Delete test
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404
+
